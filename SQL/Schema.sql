f3637639e0d3d01c7d36d4c9a4c25aca2c7bfdd5
Ylipro <n3jutu00@students.oamk.fi>
2024-11-16 18:36:28 +0200

    SQL test data

---
+++ SQL/Schema.sql
@@ -0,0 +1,15 @@
+INSERT INTO movie_genre(genre_name) VALUES 
+('drama'),('comedy'),('scifi'),('fantasy'),('action'),('triller');
+
+INSERT INTO movie (title, release_date, genre) VALUES 
+('Inception', 2010, 5),
+('The Terminator', 1984, 5),
+('Tropic Thunder', 2008, 2),
+('Borat', 2006, 2),
+('Interstellar', 2014, 1),
+('Joker', 2019, 1);
+
+INSERT INTO movie_user(username,fullname,user_password,birth_date) VALUES
+('reimarii', 'Reima Riihimäki', 'qwerty123', 1986),
+('lizzy', 'Lisa Simpson', 'abcdef', 1991 ),
+('boss', 'Ben Bossy', 'salasana', 1981 )
